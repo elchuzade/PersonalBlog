427dc353b8a03188f942142d9bbf3613bf5e26c6
Kamran Elchuzade <elchuzade@gmail.com>
2019-09-30 21:26:03 +0300

    fixed navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -46,7 +46,7 @@ class Navbar extends Component {
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/contacts">
-                  Contacts
+                  About
                 </Link>
               </li>
               {isAuthenticated && (
