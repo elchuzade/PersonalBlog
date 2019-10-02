486aaa42f4ff608110d0256abd8ef592a522ffbd
Kamran Elchuzade <elchuzade@gmail.com>
2019-10-02 23:19:56 +0300

    added a new blog model that has modular items

--- models/Blog.js
+++ models/Blog.js
@@ -35,16 +35,53 @@ const BlogSchema = new Schema(
       type: String,
       required: true
     },
-    body: {
-      type: String
-    },
     author: {
       type: String,
       required: true
     },
     tags: {
       type: String
-    }
+    },
+    body: [
+      {
+        type: {
+          // type is either image or text
+          type: String,
+          required: true
+        },
+        index: {
+          // index is used to be able to order items and swap them
+          type: Number,
+          required: true
+        },
+        image: {
+          location: {
+            type: String
+          },
+          key: {
+            type: String
+          },
+          bucket: {
+            type: String
+          },
+          originalname: {
+            type: String
+          },
+          mimetype: {
+            type: String
+          },
+          size: {
+            type: Number
+          },
+          fieldName: {
+            type: String
+          }
+        },
+        text: {
+          type: String
+        }
+      }
+    ]
   },
   { timestamps: true }
 );
